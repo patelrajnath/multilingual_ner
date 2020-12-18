bd6de2ec15b5fa7c523fe206531fa7cb1a956125
Raj Nath Patel <patelrajnath@gmail.com>
2020-12-18 17:57:25 +0000

    Add conll english data in eval script.

--- eval_ner.sh
+++ eval_ner.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 python eval_ner.py --output_dir outputs \
-    --t_labels deuutf_label_bert.txt \
-    --p_label deuutf_predict_bert.txt \
-    --text deuutf_text_bert.txt
+    --t_labels eng_label_bert.txt \
+    --p_label eng_predict_bert.txt \
+    --text eng_text_bert.txt
+#     --t_labels deuutf_label_bert.txt \
+#     --p_label deuutf_predict_bert.txt \
+#     --text deuutf_text_bert.txt
 #     --t_labels ubuntu_label.txt \
 #     --p_labels ubuntu_predict.txt \
 #     --text ubuntu_text.txt
