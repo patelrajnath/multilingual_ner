50440e303cfa43a0e70ebab4217fe7d58afc762a
Raj Nath Patel <raj.nath.patel@huawei.com>
2020-12-18 10:29:54 +0000

    Fix shell script.

--- train_ner_bert.sh
+++ train_ner_bert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python train_ner_bert.py \
-    --data_dir data/conll2003
-    --train eng.train.train.csv
-    --test eng.testb.dev.csv
+    --data_dir data/conll2003 \
+    --train eng.train.train.csv \
+    --test eng.testb.dev.csv \
     --embedding 768 \
     --hidden_layer_size 1024
     
