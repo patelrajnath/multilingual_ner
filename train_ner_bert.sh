e7531c90228689d608865108cff5bbe5d8cc01be
Raj Nath Patel <patelrajnath@gmail.com>
2020-12-18 12:24:26 +0000

    Add real test set.

--- train_ner_bert.sh
+++ train_ner_bert.sh
@@ -3,10 +3,10 @@
 python train_ner_bert.py \
     --data_dir data/conll2003 \
     --train eng.train.train.csv \
-    --test eng.testb.dev.csv \
+    --test eng.testa.dev.csv \
     --embedding 768 \
     --hidden_layer_size 1024 \
-    --max_steps 1500
+    --max_steps 15000
 #     --data_dir data/conll2003-de \
 #     --train deuutf.train.train.csv \
 #     --test deuutf.testa.dev.csv \
