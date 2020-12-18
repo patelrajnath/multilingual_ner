7910910a8009688f70c0a07866a2f068d202a6b3
Raj Nath Patel <patelrajnath@gmail.com>
2020-12-18 11:07:23 +0000

    Fix eval shell script.

--- eval_ner.sh
+++ eval_ner.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python eval_ner.py --out_dir outputs --t_labels ubuntu_labels.txt -p_labels ubuntu_predict.txt --text ubuntu_text.txt
+python eval_ner.py --output_dir outputs \
+	--t_labels ubuntu_labels.txt \
+	--p_labels ubuntu_predict.txt \
+	--text ubuntu_text.txt
