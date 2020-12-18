88af9adc51dcec5c1941ebb2df5b74f34df0d6ef
Raj Nath Patel <patelrajnath@gmail.com>
2020-12-18 12:12:28 +0000

    Update shell script.

--- eval_ner.sh
+++ eval_ner.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 python eval_ner.py --output_dir outputs \
-	--t_labels ubuntu_label.txt \
-	--p_labels ubuntu_predict.txt \
-	--text ubuntu_text.txt
+    --t_labels deuutf_label_bert.txt \
+    --p_label deuutf_predict_bert.txt \
+    --text deuutf_text_bert.txt
+#     --t_labels ubuntu_label.txt \
+#     --p_labels ubuntu_predict.txt \
+#     --text ubuntu_text.txt
--- train_ner_bert.sh
+++ train_ner_bert.sh
@@ -5,6 +5,12 @@ python train_ner_bert.py \
     --train eng.train.train.csv \
     --test eng.testb.dev.csv \
     --embedding 768 \
-    --hidden_layer_size 1024
-    
-
+    --hidden_layer_size 1024 \
+    --max_steps 1500
+#     --data_dir data/conll2003-de \
+#     --train deuutf.train.train.csv \
+#     --test deuutf.testa.dev.csv \
+#     --embedding 768 \
+#     --hidden_layer_size 1024 \
+#     --max_steps 1500 \
+#     --model_name bert-base-multilingual-cased
