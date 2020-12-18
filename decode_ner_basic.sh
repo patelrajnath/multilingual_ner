47e0b8c993834aa11d1b45ee34651ea2cdaae76f
Raj Nath Patel <patelrajnath@gmail.com>
2020-12-18 15:24:02 +0000

    Update decode shell script.

--- decode_ner_basic.sh
+++ decode_ner_basic.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 python decode_ner_basic.py \
-  --data_dir data/snips \
-  --test_text snips_test_text.txt \
-  --test_label snips_test_labels.txt \
-  --vocab words.txt --tag_set tags.txt \
+--data_dir data/nlu \
+--test_text nlu_test_text.txt \
+--test_label nlu_test_labels.txt \
+--vocab words.txt --tag_set tags.txt \
+--hidden_layer_size 1024 \
+--embedding 768 \
+--num_hidden_layers 1
 
-# --data_dir data/nlu \
-# --test_text nlu_test_text.txt \
-# --test_label nlu_test_labels.txt \
+# --data_dir data/snips \
+# --test_text snips_test_text.txt \
+# --test_label snips_test_labels.txt \
 # --vocab words.txt --tag_set tags.txt \
+# --hidden_layer_size 1024 \
+# --embedding 768 \
+# --num_hidden_layers 1
 
 # --data_dir data/ubuntu \
 # --test_text ubuntu_test_text.txt \
 # --test_label ubuntu_test_labels.txt \
-# --vocab words.txt --tag_set tags.txt \
+# --vocab words.txt --tag_set tags.txt
