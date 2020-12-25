fc35e51f60c26fc12025bd5ca0dddc9cf3e04975
Raj Nath Patel <raj.nath.patel@huawei.com>
2020-12-25 20:10:22 +0000

    Add bert tokenizer.

---
+++ bert_example.py
@@ -0,0 +1,42 @@
+import collections
+
+from transformers import BertTokenizer, BertModel, WordpieceTokenizer
+import torch
+
+from tokenizer.tokenizer_utils import load_vocab
+from tokenizer.word_piece import BasicTokenizer
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+
+# inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+# outputs = model(**inputs)
+
+# last_hidden_states = outputs.last_hidden_state
+# print(last_hidden_states.size())
+
+inputs = tokenizer.tokenize("Hello, my quantization are cute")
+inputs = ['[CLS]'] + inputs + ['[SEP]']
+print(tokenizer.convert_tokens_to_ids(inputs))
+print(inputs)
+print(tokenizer.save_vocabulary('.', 'codes'))
+# print(tokenizer.get_vocab())
+
+
+text = "Hello, my quantization are cute"
+vocab = load_vocab('tokenizer/codes-vocab.txt')
+wordpiece_tokenizer = WordpieceTokenizer(vocab, unk_token='unk')
+basic_tokenizer = BasicTokenizer()
+do_basic_tokenize = True
+split_tokens = []
+if do_basic_tokenize:
+    for token in basic_tokenizer.tokenize(text):
+
+        # If the token is part of the never_split set
+        if token in basic_tokenizer.never_split:
+            split_tokens.append(token)
+        else:
+            split_tokens += wordpiece_tokenizer.tokenize(token)
+else:
+    split_tokens = wordpiece_tokenizer.tokenize(text)
+print(split_tokens)
---
+++ tokenizer/tokenizer_utils.py
@@ -0,0 +1,64 @@
+import collections
+import unicodedata
+
+
+def load_vocab(vocab_file):
+    """Loads a vocabulary file into a dictionary."""
+    vocab = collections.OrderedDict()
+    with open(vocab_file, "r", encoding="utf-8") as reader:
+        tokens = reader.readlines()
+    for index, token in enumerate(tokens):
+        token = token.rstrip("\n")
+        vocab[token] = index
+    return vocab
+
+
+def _is_whitespace(char):
+    """Checks whether `char` is a whitespace character."""
+    # \t, \n, and \r are technically control characters but we treat them
+    # as whitespace since they are generally considered as such.
+    if char == " " or char == "\t" or char == "\n" or char == "\r":
+        return True
+    cat = unicodedata.category(char)
+    if cat == "Zs":
+        return True
+    return False
+
+
+def _is_control(char):
+    """Checks whether `char` is a control character."""
+    # These are technically control characters but we count them as whitespace
+    # characters.
+    if char == "\t" or char == "\n" or char == "\r":
+        return False
+    cat = unicodedata.category(char)
+    if cat.startswith("C"):
+        return True
+    return False
+
+
+def _is_punctuation(char):
+    """Checks whether `char` is a punctuation character."""
+    cp = ord(char)
+    # We treat all non-letter/number ASCII as punctuation.
+    # Characters such as "^", "$", and "`" are not in the Unicode
+    # Punctuation class but we treat them as punctuation anyways, for
+    # consistency.
+    if (cp >= 33 and cp <= 47) or (cp >= 58 and cp <= 64) or (cp >= 91 and cp <= 96) or (cp >= 123 and cp <= 126):
+        return True
+    cat = unicodedata.category(char)
+    if cat.startswith("P"):
+        return True
+    return False
+
+
+def _is_end_of_word(text):
+    """Checks whether the last character in text is one of a punctuation, control or whitespace character."""
+    last_char = text[-1]
+    return bool(_is_control(last_char) | _is_punctuation(last_char) | _is_whitespace(last_char))
+
+
+def _is_start_of_word(text):
+    """Checks whether the first character in text is one of a punctuation, control or whitespace character."""
+    first_char = text[0]
+    return bool(_is_control(first_char) | _is_punctuation(first_char) | _is_whitespace(first_char))
