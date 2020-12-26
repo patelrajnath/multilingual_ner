9d217389ea6dab26827db6ba6c1a576f877f21b2
Raj Nath Patel <patelrajnath@gmail.com>
2020-12-26 21:52:45 +0000

    Update eval shell script.

--- eval_ner.sh
+++ eval_ner.sh
@@ -1,38 +1,18 @@
 #!/bin/bash
 
+# prefix='eng'
+# prefix='deuutf'
+prefix='alliance'
+# prefix='wallet'
+# prefix='accounts'
+# prefix='ubuntu'
+# prefix='nlu'
+# prefix='snips'
+
+postfix='_bert'
+postfix=''
+
 python eval_ner.py --output_dir outputs \
-    --t_labels deuutf_label_bert.txt \
-    --p_label deuutf_predict_bert.txt \
-    --text deuutf_text_bert.txt
-
-#     --t_labels alliance_label_bert.txt \
-#     --p_labels alliance_predict_bert.txt \
-#     --text alliance_text_bert.txt
-
-#     --t_labels wallet_label_bert.txt \
-#     --p_labels wallet_predict_bert.txt \
-#     --text wallet_text_bert.txt
-
-#     --t_labels snips_label_bert.txt \
-#     --p_labels snips_predict_bert.txt \
-#     --text snips_text_bert.txt
-
-#     --t_labels accounts_label_bert.txt \
-#     --p_labels accounts_predict_bert.txt \
-#     --text accounts_text_bert.txt
-
-#     --t_labels nlu_label_bert.txt \
-#     --p_labels nlu_predict_bert.txt \
-#     --text nlu_text_bert.txt
-
-#     --t_labels eng_label_bert.txt \
-#     --p_label eng_predict_bert.txt \
-#     --text eng_text_bert.txt
-
-#     --t_labels deuutf_label_bert.txt \
-#     --p_label deuutf_predict_bert.txt \
-#     --text deuutf_text_bert.txt
-
-#     --t_labels ubuntu_label.txt \
-#     --p_labels ubuntu_predict.txt \
-#     --text ubuntu_text.txt
+    --t_labels ${prefix}_label${postfix}.txt \
+    --p_label ${prefix}_predict${postfix}.txt \
+    --text ${prefix}_text${postfix}.txt
