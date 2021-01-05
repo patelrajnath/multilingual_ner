d58bff0bc59bcb16d0a08e7be86c18980ad1f05d
Raj Nath Patel <patelrajnath@gmail.com>
2021-01-05 22:32:20 +0000

    Update the bert code to use Distilbert.

--- models/embedders.py
+++ models/embedders.py
@@ -11,7 +11,10 @@ glog = logging.getLogger(__name__)
 
 
 class BERTEmbedder(torch.nn.Module):
-    def __init__(self, model, config, cache_dir='./', encoder_id='bert_multilingual_embeddings', caching=False):
+    def __init__(self, model, config,
+                 cache_dir='./',
+                 encoder_id='bert_multilingual_embeddings',
+                 caching=False):
         super(BERTEmbedder, self).__init__()
         self.caching = caching
         self.config = config
@@ -119,7 +122,8 @@ class BERTEmbedder(torch.nn.Module):
 
 
 class TokenEmbedder(torch.nn.Module):
-    def __init__(self, model_class, model_name, only_embedding=True):
+    def __init__(self, model_class, model_name,
+                 only_embedding=True):
         super(TokenEmbedder, self).__init__()
         self.only_embedding = only_embedding
         if self.only_embedding:
@@ -161,9 +165,12 @@ class PretrainedEmbedder(torch.nn.Module):
                  device="cuda",
                  mode="weighted",
                  is_freeze=True,
-                 only_embedding=True):
-
+                 only_embedding=True,
+                 cache_dir='./',
+                 encoder_id='bert_multilingual_embeddings',
+                 caching=False):
         super(PretrainedEmbedder, self).__init__()
+
         config_class, model_class, tokenizer_class = MODEL_CLASSES[model_type]
         self.only_embedding = only_embedding
         self.config = config_class.from_pretrained(model_name)
@@ -172,6 +179,12 @@ class PretrainedEmbedder(torch.nn.Module):
         self.mode = mode
         self.model.to(device)
         self.model.eval()
+
+        self.caching = caching
+        self._encodings_dict_path = os.path.join(cache_dir, encoder_id)
+        if self.caching:
+            self._encodings_dict = self._load_or_create_encodings_dict()
+
         if is_freeze:
             self.freeze()
 
@@ -209,15 +222,41 @@ class PretrainedEmbedder(torch.nn.Module):
             data[2]: list, tokens type ids (for bert)
             data[3]: list, bert labels ids
         """
-        encoded_layers = self.model(**input_)
-        if self.only_embedding:
-            return encoded_layers
+        if self.caching:
+            sentences = input_[0]
+            missing_sentences = []
+            for sentence in sentences:
+                sentence_key = " ".join([str(item) for item in sentence.tolist()])
+                if sentence_key not in self._encodings_dict:
+                    missing_sentences.append(sentence)
+            if len(sentences) != len(missing_sentences):
+                glog.info(f"{len(sentences) - len(missing_sentences)} cached "
+                          f"sentences will not be encoded")
+            if missing_sentences:
+                encoded_layers = self.model(**input_)
+                encoded_layers = encoded_layers[-1]
+                if self.mode == "weighted":
+                    encoded_layers = torch.stack([a * b for a, b in zip(encoded_layers, self.bert_weights)])
+                    encoded_layers = self.bert_gamma * torch.sum(encoded_layers, dim=0)
+                for sentence, encoding in zip(missing_sentences,
+                                              encoded_layers):
+                    sentence_key = " ".join([str(item) for item in sentence.tolist()])
+                    self._encodings_dict[sentence_key] = encoding
+                # self._save_encodings_dict()
 
-        encoded_layers = encoded_layers[-1]
-        if self.mode == "weighted":
-            encoded_layers = torch.stack([a * b for a, b in zip(encoded_layers, self.bert_weights)])
-            encoded_layers = self.bert_gamma * torch.sum(encoded_layers, dim=0)
-        return encoded_layers
+            encoded_layers = torch.stack([self._encodings_dict[" ".join([str(item) for item in sentence.tolist()])]
+                                          for sentence in sentences])
+            return encoded_layers
+        else:
+            encoded_layers = self.model(**input_)
+            if self.only_embedding:
+                return encoded_layers
+
+            encoded_layers = encoded_layers[-1]
+            if self.mode == "weighted":
+                encoded_layers = torch.stack([a * b for a, b in zip(encoded_layers, self.bert_weights)])
+                encoded_layers = self.bert_gamma * torch.sum(encoded_layers, dim=0)
+            return encoded_layers
 
     def freeze(self):
         for param in self.model.parameters():
