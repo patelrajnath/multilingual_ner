57833d52c985bcfac3010507405591dd3eaa84d8
Raj Nath Patel <patelrajnath@gmail.com>
2021-01-06 21:35:26 +0000

    Add Electra and bertweet models.

--- models/constants.py
+++ models/constants.py
@@ -1,9 +1,11 @@
 from transformers import BertConfig, BertTokenizer, \
     DistilBertConfig, DistilBertTokenizer, XLMRobertaTokenizer, XLMRobertaConfig, \
-    RobertaConfig, RobertaTokenizer
+    RobertaConfig, RobertaTokenizer, BertweetTokenizer, ElectraConfig, ElectraTokenizer, LongformerConfig, \
+    LongformerTokenizer, CamembertConfig, CamembertTokenizer
 
 from models.modeling_bert import BertTokenEmbedder
 from models.modeling_distilbert import DistilBertTokenEmbedder
+from models.modeling_electra import ElectraTokenEmbedder
 from models.modeling_roberta import RobertaTokenEmbedder
 from models.modeling_xlm import XLMTokenEmbedder
 
@@ -11,5 +13,7 @@ MODEL_CLASSES = {
     "bert": (BertConfig, BertTokenEmbedder, BertTokenizer),
     "distilbert": (DistilBertConfig, DistilBertTokenEmbedder, DistilBertTokenizer),
     "xlmroberta": (XLMRobertaConfig, XLMTokenEmbedder, XLMRobertaTokenizer),
-    "roberta": (RobertaConfig, RobertaTokenEmbedder, RobertaTokenizer)
+    "roberta": (RobertaConfig, RobertaTokenEmbedder, RobertaTokenizer),
+    "bertweet": (RobertaConfig, RobertaTokenEmbedder, BertweetTokenizer),
+    "electra": (ElectraConfig, ElectraTokenEmbedder, ElectraTokenizer),
 }
---
+++ models/modeling_electra.py
@@ -0,0 +1,41 @@
+from transformers import ElectraModel, ElectraPreTrainedModel
+
+
+class ElectraTokenEmbedder(ElectraPreTrainedModel):
+    def __init__(self, config, model_name, only_embedding=True, output_hidden_states=True):
+        super(ElectraTokenEmbedder, self).__init__(config)
+        self.config = config
+        self.only_embedding = only_embedding
+        self.model = ElectraModel.from_pretrained(model_name, output_hidden_states=output_hidden_states)
+        if self.only_embedding:
+            self.model = self.model.get_input_embeddings()
+            self.model.weight.requires_grad = False
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+    ):
+        # Use only the embedding layer
+        if self.only_embedding:
+            return self.model(input_ids)
+
+        # Use the the model as encoder
+        return self.model(
+            input_ids,
+            attention_mask,
+            token_type_ids,
+            position_ids,
+            head_mask,
+            inputs_embeds,
+            output_attentions,
+            output_hidden_states,
+            return_dict,
+        )
