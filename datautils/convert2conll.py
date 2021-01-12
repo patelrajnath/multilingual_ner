62e6370d7a17a75cf0590075802b57350eaa3337
Raj Nath Patel <patelrajnath@gmail.com>
2021-01-12 17:46:43 +0000

    Add conlleval script.

--- datautils/convert2conll.py
+++ datautils/convert2conll.py
@@ -1,10 +1,10 @@
 import sys
 
 prefix = sys.argv[1]
-with open(f'../outputs/{prefix}_text_bert.txt') as ftext, \
-        open(f'../outputs/{prefix}_label_bert.txt') as flabel, \
-        open(f'../outputs/{prefix}_predict_bert.txt') as fpredict, \
-        open(f'../outputs/{prefix}_conlleval.txt', 'w', encoding='utf-8') as outfile:
+with open(f'outputs/{prefix}_text_bert.txt') as ftext, \
+        open(f'outputs/{prefix}_label_bert.txt') as flabel, \
+        open(f'outputs/{prefix}_predict_bert.txt') as fpredict, \
+        open(f'outputs/{prefix}_conlleval.txt', 'w', encoding='utf-8') as outfile:
     for linet, linel, linep in zip(ftext, flabel, fpredict):
         wordst = linet.split()
         wordsl = linel.split()
