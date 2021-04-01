d8721c1aa187be9fdeda8ad4e7c3e985fb410e1a
Raj Nath Patel <patelrajnath@gmail.com>
2021-04-01 13:53:18 +0100

    Add txt to conll converter

--- datautils/convert_txt2conll.py
+++ datautils/convert_txt2conll.py
@@ -7,8 +7,13 @@ out = sys.argv[3]
 with open(text) as ftext, \
         open(labels) as flabel, \
         open(out, 'w', encoding='utf-8') as outfile:
-
+    counter = 0
+    doc_marker = '-DOCSTART- -X- -X- O'
     for linet, linel in zip(ftext, flabel):
+        if counter % 100 == 0:
+            outfile.write(doc_marker)
+            outfile.write('\n\n')
+
         wordst = linet.split()
         wordsl = linel.split()
         for wt, wl in zip(wordst, wordsl):
@@ -18,4 +23,5 @@ with open(text) as ftext, \
 
             line = ' '.join([wt, wl]) + '\n'
             outfile.write(line)
+        counter += 1
         outfile.write('\n')
